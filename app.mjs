ca2babf521fa6e1eb92024e1cbcf19257d0571e4
Hasan Mahmud Tuhin <tuhin.mc@gmail.com>
2025-05-09 21:40:45 +0600

    added --start-maximized args on launch

--- app.mjs
+++ app.mjs
@@ -8,20 +8,35 @@ async function puppeteerShell() {
     console.log('OPENING Browser')
     const browser = await puppeteer.launch(
         {
-            headless:'shell',
-            args :[`--disable-notifications`]
+            headless: false,
+            slowMo:500,
+            args:[`--start-maximized`]
         }
     )
+    // args :[`--disable-notifications`]
     const page   = await browser.newPage()
     await page.setViewport({
         width:1920,
         height:1080,
         deviceScaleFactor:1
     })
+    const recorder = await page.screencast({path:'recording.webm'})
     await page.goto('https://www.prothomalo.com/')
     await page.screenshot({path:'prothom-alo.png', fullPage: true})
+    await recorder.stop()
     await page.close()
     await browser.close()
     console.log(`DONE. Browser Closed`)
 }
 
+/*
+ => For Screen Record ffmpeg needs to be installed on system.
+ => All recording weill be WebM format, Using VP9 Video Codec, Fram Rate 30FPS
+ 
+ Code Example
+ -------------
+ const recorder = await page.screencast({path:'recording.webm'})
+ await recoder.stop()
+
+
+*/
