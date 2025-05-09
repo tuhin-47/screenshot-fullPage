9b5f67ba331ec8f68f849b6e40828bb86b93f6ec
Hasan Mahmud Tuhin <tuhin.mc@gmail.com>
2025-05-09 19:17:02 +0600

    using argument --disable-notifications

--- app.mjs
+++ app.mjs
@@ -1,14 +1,25 @@
 import puppeteer from 'puppeteer'
 
 
+
 await puppeteerShell()
 
 async function puppeteerShell() {
     console.log('OPENING Browser')
-    const browser = await puppeteer.launch({headless:'shell'})
+    const browser = await puppeteer.launch(
+        {
+            headless:'shell',
+            args :[`--disable-notifications`]
+        }
+    )
     const page   = await browser.newPage()
-    await page.goto('https://example.com')
-    await page.screenshot({path:'example.png'})
+    await page.setViewport({
+        width:1920,
+        height:1080,
+        deviceScaleFactor:1
+    })
+    await page.goto('https://www.prothomalo.com/')
+    await page.screenshot({path:'prothom-alo.png', fullPage: true})
     await page.close()
     await browser.close()
     console.log(`DONE. Browser Closed`)
